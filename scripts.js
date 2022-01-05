5a42cd914979796f92a45c0942ebe518811b88eb
belkacem005 <92831405+belkacem005@users.noreply.github.com>
2022-01-06 00:03:36 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 function openForm(a) {
     document.getElementById(a).style.display = "block";
-
+        var x = window.matchMedia('(max-width: 768px)');
+    function toButtom(x) {
+      if (x.matches) {
+        //toButtom
+        window.scrollTo({left: 0, top: 9999, behavior: "smooth"});
+      }
+    }
+    toButtom(x);
+    x.addListener(toButtom);
     for (let i = 1; i < 6; i++) {
       if (i != a) {
         closeForm(i)
